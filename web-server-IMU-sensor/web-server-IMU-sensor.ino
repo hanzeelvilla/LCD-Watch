168baf06e6b67a6241a0694d1e596345271a9f9e
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2024-08-14 20:00:53 -0600

    WebSocket

--- web-server-IMU-sensor/web-server-IMU-sensor.ino
+++ web-server-IMU-sensor/web-server-IMU-sensor.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <ArduinoJson.h>
 #include "SensorQMI8658.hpp"
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -14,6 +15,33 @@ IMUdata acc, gyr;
 
 WiFiMulti wifiMulti;
 AsyncWebServer server(80);  // Crear un servidor en el puerto 80
+AsyncWebSocket ws("/ws");
+
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+        Serial.println("WebSocket client connected");
+    } else if (type == WS_EVT_DISCONNECT) {
+        Serial.println("WebSocket client disconnected");
+    }
+}
+
+void sendSensorData() {
+    if (qmi.getDataReady()) {
+        if (qmi.getAccelerometer(acc.x, acc.y, acc.z) && qmi.getGyroscope(gyr.x, gyr.y, gyr.z)) {
+            StaticJsonDocument<200> jsonDoc;
+            jsonDoc["accel_x"] = acc.x;
+            jsonDoc["accel_y"] = acc.y;
+            jsonDoc["accel_z"] = acc.z;
+            jsonDoc["gyro_x"] = gyr.x;
+            jsonDoc["gyro_y"] = gyr.y;
+            jsonDoc["gyro_z"] = gyr.z;
+            
+            String jsonString;
+            serializeJson(jsonDoc, jsonString);
+            ws.textAll(jsonString);
+        }
+    }
+}
 
 // Tiempo para conectarse a un access point
 const uint32_t connectTimeoutMs = 10000;
@@ -64,51 +92,47 @@ void setup() {
   qmi.enableGyroscope();
   qmi.enableAccelerometer();
 
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+
   Serial.println("Read data now...");
 
   // Definir una ruta para obtener los datos del IMU
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String data = "";
-    
-    if (qmi.getDataReady()) {
-      if (qmi.getAccelerometer(acc.x, acc.y, acc.z)) {
-        data += "ACCEL: " + String(acc.x) + ", " + String(acc.y) + ", " + String(acc.z) + "\n";
-      }
-        
-      if (qmi.getGyroscope(gyr.x, gyr.y, gyr.z)) {
-        data += "GYRO: " + String(gyr.x) + ", " + String(gyr.y) + ", " + String(gyr.z) + "\n";
-      }
-      
-      data += "Timestamp: " + String(qmi.getTimestamp()) + ", Temperature: " + String(qmi.getTemperature_C()) + " *C\n";
-    }
+    String data = R"rawliteral(
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>IMU Data</title>
+</head>
+<body>
+    <h1>IMU Data</h1>
+    <p id='accel'>Accelerometer: Loading...</p>
+    <p id='gyro'>Gyroscope: Loading...</p>
+
+    <script>
+        const ws = new WebSocket('ws://192.168.1.93/ws'); // Reemplaza con la IP de tu ESP32
+
+        ws.onmessage = function(event) {
+            const data = JSON.parse(event.data);
+            document.getElementById('accel').innerText = `Accelerometer: X=${data.accel_x}, Y=${data.accel_y}, Z=${data.accel_z}`;
+            document.getElementById('gyro').innerText = `Gyroscope: X=${data.gyro_x}, Y=${data.gyro_y}, Z=${data.gyro_z}`;
+        };
+
+        ws.onerror = function(event) {
+            console.error('WebSocket error observed:', event);
+        };
+    </script>
+</body>
+</html>
+)rawliteral";;
 
     // Enviar la respuesta con los datos del sensor
-    request->send(200, "text/plain", data);
+    request->send(200, "text/html", data);
   });
 
-  server.on("/accel", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String data = "";
-
-    if (qmi.getDataReady()) {
-      if(qmi.getAccelerometer(acc.x, acc.y, acc.z)) {
-        data += String(acc.x) + ", " + String(acc.y) + ", " + String(acc.z);
-      }
-    }
-
-    request->send(200, "text/plain", data);
-  });
-
-  server.on("/gyro", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String data = "";
-
-    if (qmi.getDataReady()) {
-      if(qmi.getGyroscope(gyr.x, gyr.y, gyr.z)) {
-        data += String(gyr.x) + ", " + String(gyr.y) + ", " + String(gyr.z);
-      }
-    }
-
-    request->send(200, "text/plain", data);
-  });
 
   server.onNotFound(notFound);
 
@@ -118,5 +142,7 @@ void setup() {
 
 void loop() {
   hayWifi();
+  sendSensorData(); // Enviar datos en tiempo real
+  ws.cleanupClients(); // Limpia clientes inactivos
   delay(1000);
 }
